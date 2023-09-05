ff2896a3ccdd3d804d042c0aafd00ba5db47584a
zich001 <48535671+zich001@users.noreply.github.com>
2023-09-05 13:46:21 +0800

    Create TU16string.py

---
+++ TU16string.py
@@ -0,0 +1,8 @@
+Ringgit = 129
+number = 10
+number1 = 129
+print(f"RM{Ringgit}")
+print(f"Binary: {number:b} {number1:b} \nHexadecimal: {number:x} {number1:x}\nDecimal: {number:d} {number1:d}")
+
+
+print(f"The binary representation for denary number {number1} is {number1:b}")
